30890560da006b185a6967b75e009dfb40f84e56
Alexander Simmerl <a.simmerl@gmail.com>
2020-10-19 17:10:04 +0200

    Remap nano base layer

--- keyboards/keebwerk/nano_slider/keymaps/xla-bento/keymap.c
+++ keyboards/keebwerk/nano_slider/keymaps/xla-bento/keymap.c
@@ -35,8 +35,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base */
     [_BASE] = LAYOUT(
         TO(_FN),
-        KC_1,    KC_2,    KC_3,
-        KC_4,    KC_5,    KC_6,    KC_0
+        KC_F13,    KC_F14,    KC_F15,
+        KC_F16,    KC_F17,    KC_F18,    KC_F19
     ),
     [_FN] = LAYOUT(
         TO(_MEDIA),
